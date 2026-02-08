0bfbd06740da55ddbe487e3f73c3606be8747668
C Scott <scott2947.dev@gmail.com>
2026-02-08 21:18:31 +0000

    added basic UDP support
    code not compliant with Liskov substitution principle so will be refactored

--- src/thea/network/server.py
+++ src/thea/network/server.py
@@ -1,57 +1,95 @@
+from abc import ABC, abstractmethod
 import socket, struct
 from thea.config import HOST, PORT
 
+class BaseServer(ABC):
 
-def start_server() -> socket.socket:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen()
+    def __init__(self):
+        self.socket : socket.socket
 
-    print(f"SERVER STARTED ON {HOST}:{PORT}")
-
-    return s
-
-
-def close_server(s : socket.socket) -> None:
-    print("SERVER CLOSED")
-    s.close()
-
-
-def accept_connection(s : socket.socket) -> socket.socket:
-    conn, addr = s.accept()
-    print(f"CONNECTED BY {addr}")
-    return conn
-
-
-def close_connection(conn : socket.socket):
-    print("CONNECTION CLOSED")
-    conn.close()
-
-
-def receive_data(conn : socket.socket) -> bytes:
-    header = conn.recv(4)
-    message_length = struct.unpack(">I", header)[0]
-    message = b""
-
-    while len(message) < message_length:
-        data = conn.recv(4096)
-        if not data:
-            break
-        message += data
     
-    print("DATA RECEIVED")
-    return message
+    @abstractmethod
+    def start_server(self):
+        pass
 
 
-def receive_text(s : socket.socket) -> str:
-    data = receive_data(s)
-    return data.decode()
+    def close_server(self) -> None:
+        self.socket.close()
+
+
+    @abstractmethod
+    def receive(self) -> bytes:
+        pass
+
+
+class TCPServer(BaseServer):
+
+    def __init__(self):
+        super().__init__()
+        self.conn : socket.socket
+
+
+    def start_server(self) -> None:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, PORT))
+        s.listen()
+
+        self.socket = s
+        print(f"TCP Server started on {HOST}:{PORT}")
+    
+
+    def close_server(self) -> None:
+        super().close_server()
+        print("TCP Server closed")
+
+
+    def accept_connection(self) -> None:
+        self.conn, addr = self.socket.accept()
+        print(f"Connected by {addr}")
+
+    
+    def close_connection(self) -> None:
+        self.conn.close()
+        print("Connection closed")
+    
+    
+    def receive(self) -> bytes:
+        header = self.conn.recv(4)
+        message_length = struct.unpack(">I", header)[0]
+        message = b""
+
+        while len(message) < message_length:
+            data = self.conn.recv(4096)
+            if not data:
+                break
+            message += data
+        
+        print("Data received")
+        return message
+    
+
+class UDPServer(BaseServer):
+
+    def start_server(self) -> None:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind((HOST, PORT))
+
+        self.socket = s
+        print(f"UDP Server started on {HOST}:{PORT}")
+    
+
+    def close_server(self) -> None:
+        super().close_server()
+        print("UDP Server closed")
+    
+
+    def receive(self) -> bytes:
+        data, addr = self.socket.recvfrom(4096)
+        print(f"Data received from {addr}")
+        return data
 
 
 if __name__ == "__main__":
-    s = start_server()
-    conn = accept_connection(s)
-    message = receive_text(conn)
-    close_connection(conn)
-    close_server(s)
-    print(message)
+    pass
+
+## Look up the Liskov substitution principle - this code needs refactoring
