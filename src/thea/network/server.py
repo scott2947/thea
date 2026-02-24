6bb384fa488fd8bc529c6603d0400df6b72cf15d
Callum Scott <scott2947.dev@gmail.com>
2026-02-24 12:44:25 +0000

    Udp support (#2)
    * added basic UDP support
    
    code not compliant with Liskov substitution principle so will be refactored
    
    * refactored code
    
    * minor modifications
    
    * added two-way communication

--- src/thea/network/server.py
+++ src/thea/network/server.py
@@ -1,57 +1,172 @@
+from abc import ABC, abstractmethod
 import socket, struct
 from thea.config import HOST, PORT
 
 
-def start_server() -> socket.socket:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen()
+class BaseServer(ABC):
 
-    print(f"SERVER STARTED ON {HOST}:{PORT}")
-
-    return s
+    def __init__(self):
+        self.socket = None
 
 
-def close_server(s : socket.socket) -> None:
-    print("SERVER CLOSED")
-    s.close()
+    @abstractmethod
+    def start_server(self):
+        pass
 
 
-def accept_connection(s : socket.socket) -> socket.socket:
-    conn, addr = s.accept()
-    print(f"CONNECTED BY {addr}")
-    return conn
+    @abstractmethod
+    def receive(self) -> bytes:
+        pass
 
 
-def close_connection(conn : socket.socket):
-    print("CONNECTION CLOSED")
-    conn.close()
-
-
-def receive_data(conn : socket.socket) -> bytes:
-    header = conn.recv(4)
-    message_length = struct.unpack(">I", header)[0]
-    message = b""
-
-    while len(message) < message_length:
-        data = conn.recv(4096)
+    def receive_string(self) -> str:
+        data = self.receive()
         if not data:
-            break
-        message += data
+            return ""
+        
+        return data.decode("utf-16")
+
+
+    @abstractmethod
+    def send(self, data: bytes) -> None:
+        pass
+
     
-    print("DATA RECEIVED")
-    return message
+    def send_string(self, message: str) -> None:
+        data = message.encode("utf-16")
+        self.send(data)
 
 
-def receive_text(s : socket.socket) -> str:
-    data = receive_data(s)
-    return data.decode()
+    def close_server(self) -> None:
+        if self.socket:
+            self.socket.close()
+            self.socket = None
+
+
+class TCPServer(BaseServer):
+
+    def __init__(self):
+        super().__init__()
+        self.conn = None
+
+
+    def start_server(self) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((HOST, PORT))
+        self.socket.listen(1)
+        print(f"TCP Server started on {HOST}:{PORT}")
+
+
+    def _recv_exactly(self, n: int) -> bytes:
+        
+        if not self.conn:
+            raise RuntimeError("Connection lost or closed by client")
+
+        data = b''
+        while len(data) < n:
+            packet = self.conn.recv(n - len(data))
+            if not packet:
+                return b''
+            data += packet
+        return data
+    
+
+    def _connect(self) -> None:
+
+        if not self.socket:
+            raise RuntimeError("Server not started. Call start_server() first")
+        
+        if self.conn is None:
+            print("TCP Server waiting for a connection")
+            self.conn, addr = self.socket.accept()
+            print(f"TCP Server connected by {addr}")
+
+
+    def receive(self) -> bytes:
+
+        self._connect()
+
+        try:
+            header = self._recv_exactly(4)
+            if not header:
+                self.close_connection()
+                return b""
+            
+            message_length = struct.unpack(">I", header)[0]
+            message = self._recv_exactly(message_length)
+            return message
+        
+        except Exception as e:
+            print(f"TCP Server receive error: {e}")
+            self.close_connection()
+            return b""
+        
+
+    def send(self, data: bytes) -> None:
+        
+        self._connect()
+
+        if not self.conn:
+            raise RuntimeError("Connection lost or closed by client")
+
+        try:
+            header = struct.pack(">I", len(data))
+            self.conn.sendall(header + data)
+        
+        except Exception as e:
+            print(f"TCP Server send error: {e}")
+            self.close_connection()
+        
+
+    def close_connection(self) -> None:
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+            print("TCP connection closed")
+
+
+    def close_server(self) -> None:
+        self.close_connection()
+        super().close_server()
+        print("TCP Server closed")
+
+
+class UDPServer(BaseServer):
+
+    def __init__(self):
+        super().__init__()
+        self.addr = None
+
+    def start_server(self) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((HOST, PORT))
+        print(f"UDP Server started on {HOST}:{PORT}")
+
+
+    def receive(self) -> bytes:
+        if not self.socket:
+            raise RuntimeError("Server not started. Call start_server() first")
+        
+        data, self.addr = self.socket.recvfrom(65535)
+        return data
+
+
+    def send(self, data: bytes) -> None:
+        
+        if not self.socket:
+            raise RuntimeError("Server not started. Call start_server() first")
+        
+        if not self.addr:
+            raise RuntimeError("Client not helloed")
+        
+        self.socket.sendto(data, self.addr)
+
+
+    def close_server(self) -> None:
+        super().close_server()
+        print("UDP Server closed")
 
 
 if __name__ == "__main__":
-    s = start_server()
-    conn = accept_connection(s)
-    message = receive_text(conn)
-    close_connection(conn)
-    close_server(s)
-    print(message)
+    pass
