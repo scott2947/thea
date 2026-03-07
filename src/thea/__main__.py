96fef7dee05a65ac479c9a98ecd2f8cc9253ec23
C Scott <scott2947.dev@gmail.com>
2026-03-07 17:17:51 +0000

    added stable camera settings

--- src/thea/__main__.py
+++ src/thea/__main__.py
@@ -1,5 +1,9 @@
+from thea.sounds import play_morse_pattern
 from thea.network.video_stream import VideoController
 from thea.utils import identify_coordinates_hsv
+
+
+play_morse_pattern(".-.-.")
 
 vc = VideoController(identify_coordinates_hsv)
 vc.start()
