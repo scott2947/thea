837d74bf3b8aaf6ce0429151689e07e447f8fe3f
C Scott <scott2947.dev@gmail.com>
2026-03-12 14:26:37 +0000

    added basic tests

--- src/thea/utils.py
+++ src/thea/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
-def calculate_average(frame: np.ndarray) -> None:
-    b, r, g = np.mean(frame, axis=(0, 1))
-    print(f"B: {b:.1f} G: {g:.1f} R: {r:.1f} ", end="\r")
+def calculate_average(frame: np.ndarray) -> tuple[float, float, float]:
+    b, g, r = np.mean(frame, axis=(0, 1))
+    return b, g, r
 
 
 if __name__ == "__main__":
---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+from thea.utils import calculate_average
+
+
+def test_calculate_average_prints_correct_values():
+    frame = np.array([[[10, 20, 30], [10, 20, 30]],
+                      [[10, 20, 30], [10, 20, 30]]], dtype=np.float64)
+
+    assert calculate_average(frame) == (10, 20, 30)
